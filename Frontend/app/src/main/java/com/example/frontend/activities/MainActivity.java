52b9eb7360c64c52ad3fd80f0e00c3ab7a73a27d
Mạnh <100762257+Qmanh1804@users.noreply.github.com>
2024-03-03 01:42:05 +0700

    careate bottom navigationbar

--- Frontend/app/src/main/java/com/example/frontend/activities/MainActivity.java
+++ Frontend/app/src/main/java/com/example/frontend/activities/MainActivity.java
@@ -1,37 +1,79 @@
 package com.example.frontend.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.example.frontend.R;
+import com.example.frontend.fragments.HomeFragment;
+import com.example.frontend.fragments.NotificationFragment;
+import com.example.frontend.fragments.ProfileFragment;
+import com.example.frontend.fragments.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
+    Fragment selectedFragment = null;
     private Button btn;
     TextView name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn = findViewById(R.id.btn);
-        name = findViewById(R.id.name);
 
-        Intent intent = getIntent();
-        String str = intent.getStringExtra("user");
-        if (str != null) {
-            name.setText(str);
-        }
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, FragmentReplacerActivity.class);
-                startActivity(intent);
-            }
-        });
+        // innit variable
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+//        selectedFragment = findViewById(R.id.fragment_layout_main);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemReselectedListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_layout_main,new HomeFragment()).commit();
+
+//        Intent intent = getIntent();
+//        String str = intent.getStringExtra("user");
+//        if (str != null) {
+//            name.setText(str);
+//        }
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, FragmentReplacerActivity.class);
+//                startActivity(intent);
+//            }
+//        });
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemReselectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    if (item.getItemId() == R.id.menu_home) {
+                        selectedFragment = new HomeFragment();
+                    } else if (item.getItemId() == R.id.menu_search) {
+                        selectedFragment = new SearchFragment();
+                    }else if (item.getItemId() == R.id.menu_post) {
+                        selectedFragment = null;
+
+                    } else if (item.getItemId() == R.id.menu_notification) {
+                        selectedFragment = new NotificationFragment();
+                    } else if (item.getItemId() == R.id.menu_profile) {
+                        selectedFragment = new ProfileFragment();
+                    }
+
+                    if(selectedFragment != null){
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_layout_main,selectedFragment).commit();
+                    }
+                    return true;
+                }
+            };
+
 }
