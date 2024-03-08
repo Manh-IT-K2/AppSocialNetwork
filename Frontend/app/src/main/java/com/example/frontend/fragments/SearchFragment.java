84fbc371ccc177458fda983ee9746a152f922c87
BYIAU <halong122002@gmail.com>
2024-03-08 17:11:59 +0700

    giao dien Tim kiem

--- Frontend/app/src/main/java/com/example/frontend/fragments/SearchFragment.java
+++ Frontend/app/src/main/java/com/example/frontend/fragments/SearchFragment.java
@@ -2,7 +2,11 @@ package com.example.frontend.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,10 +17,21 @@ import com.example.frontend.R;
 
 public class SearchFragment extends Fragment {
 
+    SearchView searchView;
+    RecyclerView recyclerView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        searchView = view.findViewById(R.id.searchView);
+        recyclerView = view.findViewById(R.id.recyclerViewSearch);
     }
 }
